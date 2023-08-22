9d32f5d2a3a642ec1e925f5e732cc1e39201259f
SpencerSedano <spencer.sv20@gmail.com>
2023-08-22 22:53:06 +0800

    smallest number

--- loops.py
+++ loops.py
@@ -1,7 +1,7 @@
 for i in [2,1,5]:
     print(i)
 
-#getting the biggest number in an array
+#Finding the biggest number in an array
 
 arr = [2,50,1,29,3,594,592,182]
 
@@ -11,3 +11,15 @@ for num in arr:
     if num > big_num:
         big_num = num
 print(f"The biggest number is: {big_num}")
+
+
+#Finding the smallest number in an array
+
+arr = [2,50,1,29,3,594,592,182]
+
+small_num = 100
+for num in arr:
+    print(num)
+    if num < small_num:
+        small_num = num
+print(f"The smallest number is: {small_num}")
